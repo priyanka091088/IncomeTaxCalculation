4838f8983366d5d1fc14f597c5915b771f39f768
Priyanka Jha <jhapriyanka06@gmail.com>
2020-08-13 17:39:18 +0530

    first commit

---
+++ IncomeTaxCalculation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IncomeTaxCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Taking age and salary for an employee
+            Console.WriteLine("Enter your age and salary");
+            int age = Convert.ToInt32(Console.ReadLine());
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            //calling TaxValue method to calculate tax for the employee
+            var person1 = new TaxCalculator(age, salary);
+            double taxValue = person1.CalculateTax();
+
+
+
+            Console.WriteLine($"The annual Income Tax value for employee of age: {age} and salary: {salary} is {taxValue}");
+        }
+    }
+}
---
+++ IncomeTaxCalculation/TaxCalculator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IncomeTaxCalculation
+{
+    class TaxCalculator
+    {
+        public TaxCalculator(int age, double salary)
+        {
+            this.Salary = salary;
+            this.Age = age;
+        }
+        private double Salary { get; set; }
+        private int Age { get; set; }
+        private double _belowSixty
+        {
+            get
+            {
+                double taxRate = 0;
+                taxRate = (Salary <= 250000) ? 0.0 : (Salary > 250000 && Salary <= 500000) ? 0.05 : (Salary > 500000 && Salary <= 1000000) ? 0.2 : 0.3;
+                return taxRate;
+            }
+        }
+
+        private double _sixtyToEighty
+        {
+            get
+            {
+                double taxRate = 0;
+                taxRate = (Salary <= 300000) ? 0.0 : (Salary > 300000 && Salary <= 500000) ? 0.05 : (Salary > 500000 && Salary <= 1000000) ? 0.2 : 0.3;
+                return taxRate;
+            }
+        }
+
+        private double _aboveEighty
+        {
+            get
+            {
+                double taxRate = 0;
+                taxRate = (Salary <= 500000) ? 0.0 : (Salary > 500000 && Salary <= 1000000) ? 0.2 : 0.3;
+                return taxRate;
+            }
+        }
+
+        public double CalculateTax()
+        {
+            double taxValue = 0;
+
+            if (Age < 60)
+            {
+                taxValue = (Salary < 500000) ? (_belowSixty * Salary) : (Salary >= 500000 && Salary < 1000000) ?
+                 (_belowSixty * (Salary - 500000) + 12500) : ((_belowSixty * (Salary - 1000000)) + 112500);
+            }
+            else if (Age >= 60 && Age < 80)
+            {
+
+                taxValue = (Salary <= 500000) ? (_sixtyToEighty * Salary) : (Salary > 500000 && Salary < 1000000) ?
+                    ((_sixtyToEighty * (Salary - 500000)) + 10000) : ((_sixtyToEighty * (Salary - 1000000)) + 110000);
+            }
+            else if (Age >= 80)
+            {
+                taxValue = (Salary <= 1000000) ? (_aboveEighty * Salary) : ((_aboveEighty * (Salary - 1000000)) + 100000);
+            }
+            else
+            {
+                return 0;
+            }
+            return taxValue;
+        }
+    }
+}
